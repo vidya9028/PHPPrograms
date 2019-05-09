28471eb39cad5d777aacc4a0231c0ad99a335b9d
vidya9028 <vidyasagar94055@gmail.com>
2019-05-09 08:59:05 +0530

    Modified Util file for AddressBook

--- OOPPrograms/Util.php
+++ OOPPrograms/Util.php
@@ -1,4 +1,6 @@
 <?php
+include "Person.php";
+
 class Util{
     
     /**
@@ -62,6 +64,11 @@ class Util{
         return $jsondata;
     }
 
+    public static function read_jsonFile($file)
+    {
+        return json_decode(file_get_contents($file));
+    }
+
     /**
      * Method for displaying each value of inventoryDatamanagement
      */
@@ -90,12 +97,12 @@ class Util{
      * Method for Entering Moblie Number
      * Number Validation
      */
-    public static function inputMobileNumber()
+    public static function validateMobileNumber()
     {
         $number = readline(" ");
         while(!(preg_match('/^[0-9]{2}-[0-9]{10}$/',$number))){
             echo "Invalid Input! Please Enter Valid Number: ";
-            $number = Util::inputMobileNumber();
+            $number = Util::validateMobileNumber();
         }
         return $number;
     }
@@ -115,5 +122,165 @@ class Util{
         }
         return $array;
     }
+
+    /**
+     * Method for Entering Moblie Number
+     * Number Validation
+     */
+    public static function inputMobileNumber()
+    {
+        $mobileNumber = readline(" ");
+        while(!(preg_match('/[0-9]{10}$/',$mobileNumber))){
+            echo "Invalid Input! Please Enter Valid Number: ";
+            $mobileNumber = Util::inputMobileNumber();
+        }
+        return $mobileNumber;
+    }
+    /**
+     * Method for adding person details in addressbook
+     */
+    public static function addPerson(&$addressBook)
+    {
+        $person = new Person();
+
+        echo "\nEnter the First Name: ";
+        $person->firstName = Util::string_input();
+
+        echo "\nEnter the Last Name: ";
+        $person->lastName = Util::string_input();
+
+        echo "\nEnter the Address: ";
+        $person->address = Util::string_input();
+
+        echo "\nEnter the City: ";
+        $person->city = Util::string_input();
+
+        echo "\nEnter the State: ";
+        $person->state = Util::string_input();
+
+        echo "\nEnter the ZipCode: ";
+        $person->zipCode = Util::user_integerInput();
+
+        echo "\nEnter the Mobile Number: ";
+        $person->mobileNumber = Util::inputMobileNumber();
+
+        $addressBook[] = $person;
+        return $addressBook;
+    }
+
+    /**
+     * Method for Edit data of persons
+     */
+    public static function editData(&$person){
+        echo "1.Change Address\n2.Change Mobile Number\nEnter Your Choice: ";
+        $choice = Util::user_integerInput();
+        
+        switch($choice){
+            case 1:
+                echo "Enter the Address: ";
+                $person->address = Util::string_input();
+
+                echo "\nEnter the City: ";
+                $person->city = Util::string_input();
+
+                echo "\nEnter the State: ";
+                $person->state = Util::string_input();
+
+                echo "\nEnter the ZipCode: ";
+                $person->zipCode = Util::user_integerInput();
+                
+                echo "Address Changed Successfully!\n";
+                return $person;
+                break;
+            
+            case 2:
+                echo "Enter the Mobile Number: ";
+                $person->mobileNumber = Util::inputMobileNumber();
+                echo "Mobile Number Changed Successfully!\n";
+                return $person;
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    /**
+     * Method for deleting person
+     */
+    public static function deletePerson(&$array)
+    {
+        $key = Util::searchPerson($array);
+        if($key > -1){
+            array_splice($array,$key,1);
+            echo "Person Deleted Succesfully!\n";
+        }
+        else{
+            echo "Person Not Found!\n";
+        }
+    }
+
+    /**
+     * Method for searching for a specific person
+     * If found return index of person else -1
+     */
+    public static function searchPerson(&$array)
+        {
+            //taking first an last name to search
+            echo "Enter the first Name: ";
+            $fname = Util::string_input();
+            echo "Enter the last Name: ";
+            $lname = Util::string_input();
+         
+            for ($i = 0; $i < count($array); $i++) 
+            {
+                if ($array[$i]->firstName == $fname) 
+                {
+                    if ($array[$i]->lastName  == $lname) 
+                    {
+                        return $i;
+                    }
+                }
+            }
+            return -1;
+        }
+    
+    /**
+     * Method for Display address Book
+     */
+    public static function displayAddressBook($array)
+    {
+        
+        foreach($array as $person){
+            echo sprintf("%s %s\n%s\n%s, %s\nZipCode-%u\nMobile Number-%u\n\n",$person->firstName,$person->lastName,$person->address,$person->city,$person->state,$person->zipCode,$person->mobileNumber);
+        }
+    }
+
+    /**
+     * Method for sorting persons according lastname or Zipcode
+     */
+    public static function sortAddressBook(&$array,$prop)
+    {
+        for($i=1;$i<count($array);$i++){
+            $j = $i-1;
+            $temp = $array[$i];
+            while($j>=0 && $array[$j]->{$prop} >= $temp->{$prop}){
+                $array[$j+1] = $array[$j];
+                $j--;
+            }
+            $array[$j+1] = $temp;
+            
+        }   
+    }
+
+    /**
+     * Method for save data in json file
+     */
+    public static function save(&$addressBook)
+    {
+        file_put_contents("AddressBooklist.json",json_encode($addressBook));
+    }
+
+    
 }
 ?>
