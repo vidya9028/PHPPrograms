04dee94ca2fb43a975e0667bea38d64209d371bb
vidya9028 <vidyasagar94055@gmail.com>
2019-06-04 14:32:48 +0530

    Uploading forgot password view file

---
+++ RESTAPICallsWithCodeIgniter/application/views/FrontEnd/forgot_password.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Reset Password</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+</head>
+<body>
+<div class="container">
+  <br />
+  <h3 align="center">Forgot Password</h3>
+  <br />
+  <div class="panel panel-default">
+  <div class="panel-body">
+  <?php
+    if($this->session->flashdata('message')){
+      echo '
+           <div class="alert alert-success">
+           '.$this->session->flashdata("message").'
+           </div>';
+    }
+  ?>
+  
+  <form method="post" action="<?php echo base_url(); ?>login/forgot_password">
+  <div class="form-group">
+      <label>Enter Registered Email Address</label>
+      <input type="text" name="user_email" class="form-control" value="<?php echo set_value('user_email'); ?>" />
+      <span class="text-danger"><?php echo form_error('user_email'); ?></span>
+  </div>
+  <div class="form-group">
+      <input type="submit" name="submit" value="Submit" class="btn btn-info" />
+  </div>
+  </form>
+  </div>
+  </div>
+  </div>
+</body> 
+</html>
