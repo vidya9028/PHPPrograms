288c599995d8cc14cf77a1b468acd8e4d0a55495
vidya9028 <vidyasagar94055@gmail.com>
2019-06-04 14:33:41 +0530

    Uploading reset password view file

---
+++ RESTAPICallsWithCodeIgniter/application/views/FrontEnd/reset_password.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <title>Reset Password</title>
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+</head>
+
+<body style = "background-color:LightGray">
+ <div class="container">
+  <br />
+  <h3 align="center">Reset Password</h3>
+  <br />
+  <div class="panel panel-default">
+   <div class="panel-heading" style = "color:red">Reset Password</div>
+   <div class="panel-body">
+    <form method="post" action="<?php echo base_url(); ?>login/reset_password">
+     <div class="form-group">
+      <label>Enter New Password</label>
+      <input type="password" name="user_password" class="form-control" value="<?php echo set_value('user_password'); ?>" />
+      <span class="text-danger"><?php echo form_error('user_password'); ?></span>
+     </div>
+     <div class="form-group">
+      <label>Confirm Password</label>
+      <input type="password" name="confirm_password" class="form-control" value="<?php echo set_value('confirm_password'); ?>" />
+      <span class="text-danger"><?php echo form_error('confirm_password'); ?></span>
+     </div>
+     <div class="form-group">
+      <input type="submit" name="reset password" value="Submit" class="btn btn-info" />
+     </div>
+    </form>
+   </div>
+  </div>
+ </div>
+</body>
+</html>
