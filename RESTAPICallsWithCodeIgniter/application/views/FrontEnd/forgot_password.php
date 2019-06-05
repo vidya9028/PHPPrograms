c3ff0e967465198be1f65989cf0e987e7ff75a4d
vidya9028 <vidyasagar94055@gmail.com>
2019-06-05 10:34:52 +0530

    removed alert message script

--- RESTAPICallsWithCodeIgniter/application/views/FrontEnd/forgot_password.php
+++ RESTAPICallsWithCodeIgniter/application/views/FrontEnd/forgot_password.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Reset Password</title>
+<title>Forgot Password</title>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
 </head>
 <body>
@@ -11,14 +11,6 @@
   <br />
   <div class="panel panel-default">
   <div class="panel-body">
-  <?php
-    if($this->session->flashdata('message')){
-      echo '
-           <div class="alert alert-success">
-           '.$this->session->flashdata("message").'
-           </div>';
-    }
-  ?>
   
   <form method="post" action="<?php echo base_url(); ?>login/forgot_password">
   <div class="form-group">
