9a1fffe08fd7dfeeae74bc490db18f1bc4737cbc
vidya9028 <vidyasagar94055@gmail.com>
2019-06-06 15:22:18 +0530

    added alert message Script

--- RESTAPICallsWithCodeIgniter/application/views/FrontEnd/forgot_password.php
+++ RESTAPICallsWithCodeIgniter/application/views/FrontEnd/forgot_password.php
@@ -11,7 +11,15 @@
   <br />
   <div class="panel panel-default">
   <div class="panel-body">
-  
+  <?php
+        if($this->session->flashdata('message'))
+        {
+            echo '
+             <div class="alert alert-success">
+             '.$this->session->flashdata("message").'
+             </div>';
+        }
+    ?>
   <form method="post" action="<?php echo base_url(); ?>login/forgot_password">
   <div class="form-group">
       <label>Enter Registered Email Address</label>
